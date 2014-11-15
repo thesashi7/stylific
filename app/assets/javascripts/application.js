81eaa3bef8c74b4b467c4db5adedcb9f9ef97473
alexlcs <alexlcs.git@gmail.com>
2014-11-14 23:42:11 -0800

    Front_page++

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,3 +22,18 @@ $(document).ready(function(){
 		$(this).toggleClass("red-border");
 	});
 });
+
+$(function() {
+		  $('a[href*=#]:not([href=#])').click(function() {
+		    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+		      var target = $(this.hash);
+		      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+		      if (target.length) {
+		        $('html,body').animate({
+		          scrollTop: target.offset().top
+		        }, 500);
+		        return false;
+		      }
+		    }
+		  });
+		});
