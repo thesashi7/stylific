e7305e332c7d0ac50418eac6857267e820ab1c08
Danial Shahbaz <Stylifyc@DanialsMacright.att.net>
2014-11-24 19:32:47 -0800

    Add files needed for calendar.

---
+++ app/controllers/stylists_controller.rb
@@ -0,0 +1,6 @@
+class StylistsController < ApplicationController
+
+	def post
+		
+	end
+end
---
+++ test/controllers/stylists_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class StylistsControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
